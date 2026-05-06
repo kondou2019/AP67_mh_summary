25bff2603fb9acdd1231bb8e0826e481c427fb03
kondou <kondou@localdomain>
2026-05-06 10:06:07 +0900

    :bookmark: 開始;0.2

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ from src.task_categorize import MhTaskCategorize
 from src.task_sort import MhTaskSort
 from src.task_utl import format_mht_list
 
-__VERSION__ = "0.1.0"
+__VERSION__ = "0.1.255"
 
 
 class MainWindow:
